962ac1cec01a3158571a1493400b8f65077e1e04
Emily Chua <emily.chua@bc.edu>
2024-01-23 09:12:11 -0500

    Improved diel analysis

--- dielAnalysis.m
+++ dielAnalysis.m
@@ -10,7 +10,7 @@
 % January 2023
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-clear all;close all;clc
+clear;close all;clc
 
 rootpath = 'G:\My Drive\Postdoc\Work\SMIIL\';
 
@@ -18,21 +18,78 @@ rootpath = 'G:\My Drive\Postdoc\Work\SMIIL\';
 cd([rootpath,'open-water-platform-data\gull\cleaned'])
 load gull-cleaned.mat
 
-dat_all = sonde1_cleaned;
-
 cd([rootpath,'physical-data\wind-speed'])
-load metData.mat
+load windSpeed.mat
 
+cd([rootpath,'physical-data\par'])
+load par.mat
+
+% Retime cleaned Gull met data to same datetimes as sonde data 
 newTimes = sonde1_cleaned.datetime_utc(1):minutes(10):sonde1_cleaned.datetime_utc(end);
-metDat2 = retime(metDat,newTimes,'mean'); % Calculate mean of values in each time bin
+metDat_rt = retime(metDat_cleaned,newTimes,'mean'); 
 
-dat = synchronize(dat_all,metDat2);
+%====Find where there are gaps in Gull met station wind speed data=========
+% Retime NOAA data to same datetimes as sonde data 
+noaaDat_rt = retime(noaaDat,newTimes,'previous');
 
-dat = rmmissing(dat,'DataVariables',{'DO_conc','wspd'});
+for i = 1:height(metDat_rt.datetime_utc)
+    nid = find(isnan(metDat_rt.wspd));
+    id(1:length(nid),1) = nid;
+end
 
 figure(1),clf
-plot(dat.datetime_utc,dat.DO_conc,'.')
-ylabel('DO conc')
+plot(metDat_rt.datetime_utc,metDat_rt.wspd,'.-')
+hold on
+plot(noaaDat_rt.date(id),noaaDat_rt.wspd_avg(id),'or','MarkerSize',4)
+ylabel('Wind Speed (m/s)')
+legend('Gull Met Station','NOAA Daily Mean')
+title('Gap-Filled Wind Speed Data')
+
+%====Gap fill Gull met station wind speed data using NOAA daily means======
+ind_nan = find(isnan(metDat_rt.wspd));
+metDat_rt.wspd(ind_nan) = noaaDat_rt.wspd_avg(ind_nan);
+
+%====Find where there are gaps in Gull met station air T data==============
+% Retime PAR data to same datetimes as sonde data 
+parDat_rt = retime(parDat,newTimes,'previous');
+
+for i = 1:height(metDat_rt.datetime_utc)
+    nid = find(isnan(metDat_rt.Tair));
+    id(1:length(nid),1) = nid;
+end
+
+figure(2),clf
+plot(metDat_rt.datetime_utc,metDat_rt.Tair,'.-')
+hold on
+plot(parDat_rt.datetime_utc(id),parDat_rt.Tair(id),'or','MarkerSize',4)
+ylabel('Air T (^oC)')
+legend('Gull Met Station','PAR Dataset')
+title('Gap-Filled Air Temperature Data')
+
+%====Gap fill Gull met station air T data using PAR data===================
+ind_nan = find(isnan(metDat_rt.Tair));
+metDat_rt.Tair(ind_nan) = parDat_rt.Tair(ind_nan);
+
+%====Gap fill Gull met station atm p data using global mean value==========
+patm_mean = mean(metDat_cleaned.patm,'omitmissing');
+ind_nan = find(isnan(metDat_rt.patm));
+metDat_rt.patm(ind_nan) = patm_mean;
+
+figure(3),clf
+plot(metDat_rt.datetime_utc,metDat_rt.patm,'.-')
+hold on
+plot(metDat_rt.datetime_utc(ind_nan),metDat_rt.patm(ind_nan),'or','MarkerSize',4)
+ylabel('p_{atm} (hPa)')
+legend('Gull Met Station','Gull Met Station Mean')
+title('Gap-Filled Atmospheric Pressure Data')
+
+% Horizontally concatenate sonde and met data (already have common time vector)
+dat = synchronize(sonde1_cleaned,metDat_rt);
+dat = rmmissing(dat,'DataVariables',{'DO_conc','wspd'});
+
+% Check how many days of data remain in synchronized data
+dataPoints = daysact(dat.datetime_utc(1),dat.datetime_utc);
+wholeDays = length(unique(floor(dataPoints)));
 
 %% STEP 1
 %====Calculate DO concentration at equilibrium (DO_sat)=================
@@ -40,7 +97,7 @@ DO_sat = O2sol(dat.salinity,dat.temperature);     % [umol kg-1]
 % Use Gibbs Seawater toolbox to calculate seawater density
 rho_sw = gsw_rho(dat.salinity,dat.temperature,dat.p); % [kg m-3]
 % Convert DO_sat units
-DO_sat = DO_sat.*rho_sw/1000;   % [mmol m-3]
+DO_sat = DO_sat.*rho_sw/1000;       % [mmol m-3]
 % Convert DO_conc units
 DO_conc = dat.DO_conc*1000/1000;    % [mmol m-3]
 % Calculate the percent oxygen saturation
@@ -50,9 +107,11 @@ DO_per_sat = dat.DO_conc./DO_sat*100;   % [%]
 %====Calculate gas exchange at air-water interface=======================
 % Caffrey/Beck (uses "reaeration coefficient", ka)
 
-H = 1.593;      % Mean water depth [m] -- CHANGE THIS!!
-rho_a = 1.293;  % Density of air [kg m-3]
-T = dat.temperature;
+H = mean(dat.depth,'omitnan');      % Mean water depth [m] -- add a constant (e.g., 0.5 m) to account for height of sonde above bottom?
+Tkair = dat.Tair + 273.15;          % Absolute air temperature [K]
+R_specific = 287.0500676;           % Specific gas constant for dry air [J kg-1 K-1]
+rho_a = dat.patm*100 ./ (R_specific*Tkair); % Density of dry air [kg m-3]
+T = dat.temperature;                % Water temperature [degC]
 S = dat.salinity;
 u = dat.wspd;
 
@@ -63,7 +122,7 @@ end
 Vw = Uw./rho_sw;    % Kinematic viscosity [m2 s-1]
 kB = 1.3806503E-23; % Boltzmann's constant [m2 kg s-2 K-1]
 R0 = 1.72E-10;      % Radius of O2 molecule [m]
-Tk = T + 273.15;    % [K]
+Tk = T + 273.15;    % Absolute temperature [K]
 ka = 1/H * 0.1706 .* (kB*Tk./(4*rho_sw.*Vw.^2.*R0)).^0.5 .* (rho_a./rho_sw).^0.5 .* u.^1.81;
 
 % Air-water flux
@@ -81,32 +140,96 @@ dCdt = nan(length(dat.DO_conc),1);
 dCdt(2:end,1) = diff(dat.DO_conc) ./ hours(diff(dat.datetime_utc));  % [mmol m-3 h-1]
 
 % Get day/night indices based on lat and lon and Hilary's indexDayNight function
-% Or use PAR data for SMIIL analysis
-lat = 39.04;
-lon = -74.76;
-UTCoffset = -5; % Need to change to -4/-5 depending if it's DST or not
+% Or use PAR data for SMIIL analysis??
+lat = 39.08;
+lon = -74.78;
 time_in = dat.datetime_utc;
 tol = 0;
-[dayind,nightind] = indexDayNight_EJC(lat,lon,UTCoffset,time_in,tol);
 
-figure,clf;
-plot(dat.datetime_utc(dayind),dat.DO_conc(dayind),'b.')
-hold on;
-plot(dat.datetime_utc(nightind),dat.DO_conc(nightind),'k.');
-ylabel('DO Concentration (mmol m^{-3})')
-legend('Day','Night')
+% Manually define DST start and end times
+start1 = datenum('03/14/2021 02:00','mm/dd/yyyy HH:MM');
+end1 = datenum('11/07/2021 02:00','mm/dd/yyyy HH:MM');
+start2 = datenum('03/13/2022 02:00','mm/dd/yyyy HH:MM');
+end2 = datenum('11/6/2022 02:00','mm/dd/yyyy HH:MM');
+start3 = datenum('03/12/2023 02:00','mm/dd/yyyy HH:MM');
+end3 = datenum('11/5/2023 02:00','mm/dd/yyyy HH:MM');
 
-daystart = nightind(find(diff(nightind)>1)) + 1;
-dayend = dayind(find(diff(dayind)>1));
+% Calculate all day/night indices assuming EDT
+UTCoffset = -4; % [h]
+[dayind_edt,nightind_edt] = indexDayNight_EJC(lat,lon,UTCoffset,time_in,tol);
+ind_edt = [dayind_edt;nightind_edt];
+
+% Calculate all day/night indices assuming EST
+UTCoffset = -5; % [h]
+[dayind_est,nightind_est] = indexDayNight_EJC(lat,lon,UTCoffset,time_in,tol);
+
+% Find closest indices in datetime_utc that match DST start and end times
+[~,edt_end1] = min(abs(datenum(dat.datetime_utc)-end1));    % Note: This date isn't close due to the data gap in fall 2021/winter 2022
+[~,edt_start2] = min(abs(datenum(dat.datetime_utc)-start2));
+[~,edt_end2] = min(abs(datenum(dat.datetime_utc)-end2));
+[~,edt_start3] = min(abs(datenum(dat.datetime_utc)-start3));
+[~,edt_end3] = min(abs(datenum(dat.datetime_utc)-end3));
+
+dayind_edt = table(dayind_edt,repmat({'day'},[length(dayind_edt),1]));
+dayind_edt.Properties.VariableNames = ["index","day/night"];
+nightind_edt = table(nightind_edt,repmat({'night'},[length(nightind_edt),1]));
+nightind_edt.Properties.VariableNames = ["index","day/night"];
+ind_edt = [dayind_edt; nightind_edt];
+ind_edt = sortrows(ind_edt);
+
+dayind_est = table(dayind_est,repmat({'day'},[length(dayind_est),1]));
+dayind_est.Properties.VariableNames = ["index","day/night"];
+nightind_est = table(nightind_est,repmat({'night'},[length(nightind_est),1]));
+nightind_est.Properties.VariableNames = ["index","day/night"];
+ind_est = [dayind_est; nightind_est];
+ind_est = sortrows(ind_est);
+
+% Define periods for whether it is DST or not
+t1 = ind_edt(1:edt_end1,:);              % EDT period
+t2 = ind_est(edt_end1+1:edt_start2-1,:); % EST period
+t3 = ind_edt(edt_start2:edt_end2,:);     % EDT period
+t4 = ind_est(edt_end2+1:edt_start3-1,:);   % EST period
+t5 = ind_edt(edt_start3:end,:);          % EDT period
+
+allind = [t1;t2;t3;t4;t5];
+
+dayind = strcmp(allind.("day/night"),'day');
+nightind = strcmp(allind.("day/night"),'night');
+
+% For plotting, create datetimes for when EDT begins/ends each year during data record
+end1_dt = datetime(end1,'ConvertFrom','datenum','TimeZone','UTC');
+start2_dt = datetime(start2,'ConvertFrom','datenum','TimeZone','UTC');
+end2_dt = datetime(end2,'ConvertFrom','datenum','TimeZone','UTC');
+start3_dt = datetime(start3,'ConvertFrom','datenum','TimeZone','UTC');
+end3_dt = datetime(end3,'ConvertFrom','datenum','TimeZone','UTC');
+
+figure(3),clf
+plot(dat.datetime_utc(dayind),dat.DO_conc(dayind),'.b','MarkerSize',12)
+hold on
+plot(dat.datetime_utc(nightind),dat.DO_conc(nightind),'.k','MarkerSize',12)
+xline(end1_dt,'--','label','DST Ends')
+xline(start2_dt,'--','label','DST Starts')
+xline(end2_dt,'--','label','DST Ends')
+xline(start3_dt,'--','label','DST Starts')
+% xline(end3_dt,'--','label','Daylight Saving Time Ends','labelhorizontalalignment','left')
+ylabel('DO conc (\mumol/L)')
+
+% Find the indices for when each day starts and stops
+daystart = find(diff(dayind) == 1) + 1;
+dayend = find(diff(dayind) == -1);
+
+% Length of each day
 daylength = dat.datetime_utc(dayend(2:end)) - dat.datetime_utc(daystart(1:end));
 daylength = hours(daylength);
+
+% figure,clf;plot(dat.datetime_utc(daystart(2:end)),daylength,'.')
 
 % Hourly rates of respiration and net production
 for i = 1:length(daylength)
     % During night hours, P = 0
-    R_hourly(i,1) = mean(dCdt(dayend(i):daystart(i)) - D(dayend(i):daystart(i)),'omitnan'); % Hourly rate of respiration; [mmol m-3 h-1]
+    R_hourly(i,1) = mean(dCdt(dayend(i):daystart(i)) - D(dayend(i):daystart(i)),'omitnan');     % Hourly rate of respiration; [mmol m-3 h-1]
     % During day hours, P != 0
-    P_hourly(i,1) = mean(dCdt(daystart(i):dayend(i+1)) - D(daystart(i):dayend(i+1)),'omitnan');     % Hourly rate of net production; [mmol m-3 h-1]
+    P_hourly(i,1) = mean(dCdt(daystart(i):dayend(i+1)) - D(daystart(i):dayend(i+1)),'omitnan'); % Hourly rate of net production; [mmol m-3 h-1]
 end
 
 % Daily rates
@@ -127,3 +250,6 @@ plot(dat.datetime_utc(daystart),NEM,'r.-','MarkerSize',12,'LineWidth',2)
 ylabel('mmol O_2 m^{-2} d^{-1}','FontSize',14)
 legend({'GPP','ER','NEM'},'FontSize',14)
 set(gca,'FontSize',14,'LineWidth',2)
+title('Gull - BC Sonde')
+
+cd('G:\My Drive\Postdoc\Work\SMIIL\figures\diel-analysis-figures')
---
+++ getWindSpeeds.m
@@ -0,0 +1,160 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% getWindSpeeds.m
+% This script creates output tables (.mat) of wind speed data (1) measured at
+% the Gull Island met station and (2) downloaded from the NOAA CDO.
+%
+% AUTHOR:
+% Emily Chua 
+% 
+% DATE:
+% January 2023
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear;close all;clc
+
+cd('G:\My Drive\Postdoc\Work\SMIIL\physical-data\wind-speed')
+
+%====Import wind speed data measured at Gull Met Station===================
+dat1 = readtable('Gull_Met_210914_220625.csv');
+varNames = ["datetime_utc","Tair","wspd","wdir","patm","rhumid","source"];
+varUnits = ["","degC","m/s","","hPa","",""];
+dat1.Properties.VariableNames = varNames;
+dat1.Properties.VariableUnits = varUnits;
+
+dat2 = readtable('Gull_Met_WL_220625_220824.xlsx');
+varNames = ["datetime_utc","wspd","wdir","Tair","rhumid","patm"];
+varUnits = ["","m/s","","degC","","hPa"];
+dat2.Properties.VariableNames = varNames;
+dat2.Properties.VariableUnits = varUnits;
+dat2.datetime_utc = datetime(dat2.datetime_utc);
+
+dat3 = readtable('Gull_Met_WL_220824_221008.xlsx');
+dat3.Properties.VariableNames = varNames;
+dat3.Properties.VariableUnits = varUnits;
+dat3.datetime_utc = datetime(dat3.datetime_utc);
+
+dat4 = readtable('Gull_Met_WL_221008_to_230207.xlsx');
+dat4.Properties.VariableNames = varNames;
+dat4.Properties.VariableUnits = varUnits;
+dat4.datetime_utc = datetime(dat4.datetime_utc);
+
+% Concatenate data tables
+dat1 = dat1(:,{'datetime_utc','wspd','wdir','Tair','rhumid','patm'});   % Restructure table 1
+metDat_orig = [dat1;dat2;dat3;dat4];
+metDat_orig.datetime_utc.TimeZone = 'UTC';
+
+metDat_orig = table2timetable(metDat_orig);
+
+clear dat1 dat2 dat3 dat4 varNames varUnits
+
+%====QC the met data=======================================================
+% Visually assess the data
+figure(2),clf;
+t = tiledlayout(3,1);
+ax1 = nexttile;
+plot(metDat_orig.datetime_utc,metDat_orig.patm,'.');ylabel('p_{atm}')
+ax2 = nexttile;
+plot(metDat_orig.datetime_utc,metDat_orig.Tair,'.');ylabel('T_{air} (^oC)')
+ax3 = nexttile;
+plot(metDat_orig.datetime_utc,metDat_orig.wspd,'.');ylabel('Wind speed (m/s)')
+title(t,'Gull Island Met Station','fontsize',16)
+t.TileSpacing = 'compact';
+
+% Link the axes
+linkaxes([ax1,ax2,ax3],'x')     
+
+% Create a table of flags for each sonde
+metDat_cleaned = metDat_orig;
+
+% Start with everything as passing (flag = 1)
+flags = ones(height(metDat_orig),width(metDat_orig));
+flags = array2table(flags);
+varNames = ["Tair","wspd","wdir","patm","rhumid"];
+flags.Properties.VariableNames = varNames;
+
+% Gross Range Test
+% INPUTS
+p_low = 900;      % Lower limit (hPa)
+p_high = 1100;  % Upper limit (hPa)
+
+ind_low = find(metDat_orig.patm < p_low);
+ind_high = find(metDat_orig.patm > p_high);
+ind_grossRange = [ind_low;ind_high];
+
+% Clean data
+% Discard points that failed Gross Range Test
+metDat_cleaned(ind_grossRange,:) = {NaN};
+
+% Flag all failed points
+flags(ind_grossRange,:) = {4};
+
+% Highlight the flagged points
+figure(3),clf;
+t = tiledlayout(3,1);
+ax1 = nexttile;
+plot(metDat_orig.datetime_utc,metDat_orig.patm,'.','MarkerSize',6);ylabel('p_{atm}')
+hold on
+plot(metDat_orig.datetime_utc(ind_grossRange),metDat_orig.patm(ind_grossRange),'or','MarkerSize',8)
+legend('Original Data','Flagged Points')
+ax2 = nexttile;
+plot(metDat_orig.datetime_utc,metDat_orig.Tair,'.','MarkerSize',6);ylabel('T_{air} (^oC)')
+hold on
+plot(metDat_orig.datetime_utc(ind_grossRange),metDat_orig.Tair(ind_grossRange),'or','MarkerSize',8)
+ax3 = nexttile;
+plot(metDat_orig.datetime_utc,metDat_orig.wspd,'.','MarkerSize',6);ylabel('Wind speed (m/s)')
+hold on
+plot(metDat_orig.datetime_utc(ind_grossRange),metDat_orig.wspd(ind_grossRange),'or','MarkerSize',8)
+title(t,'Gull Island Met Station - Data QC','fontsize',16)
+t.TileSpacing = 'compact';
+
+% Link the axes
+linkaxes([ax1,ax2,ax3],'x')     
+
+%====Import climatological data downloaded from NOAA=======================
+% https://www.ncdc.noaa.gov/cdo-web/datasets/GHCND/stations/GHCND:USW00013724/detail
+noaaDat = readtable('noaa_atlanticCity.csv');
+varNames = ["station","name","latitude","longitude","elevation","date","wspd_avg","precip","Tmax","Tmin"];
+varUnits = ["","","deg","deg","m","","m/s","mm","degC","degC"];
+noaaDat.Properties.VariableNames = varNames;
+noaaDat.Properties.VariableUnits = varUnits;
+noaaDat.date.TimeZone = 'UTC';
+
+noaaDat = table2timetable(noaaDat);
+
+clear varNames varUnits
+
+%====Compare Gull and NOAA daily mean wind speeds==========================
+% Find daily means of cleaned wind speeds
+metDat_dailyMean = retime(metDat_cleaned,'daily','mean');
+
+figure(4),clf
+plot(metDat_dailyMean.datetime_utc,metDat_dailyMean.wspd,'.-')
+hold on
+plot(noaaDat.date,noaaDat.wspd_avg,'.-')
+ylabel('Daily Mean Wind Speed (m/s)')
+legend('Gull Met Station (cleaned)','NOAA - Atlantic City Marina')
+
+% Find date range in NOAA data that spans Gull Met data
+[~,start] = ismember(metDat_dailyMean.datetime_utc(1),noaaDat.date);
+[~,stop] = ismember(metDat_dailyMean.datetime_utc(end),noaaDat.date);
+
+% Plot linear regression between cleaned Gull Met and NOAA data
+tbl = table(metDat_dailyMean.wspd,noaaDat.wspd_avg(start:stop));
+mdl = fitlm(tbl,'linear');
+
+% Create equation string for plot
+eqn = ['y = ',num2str(mdl.Coefficients.Estimate(2),2),'x + ',num2str(mdl.Coefficients.Estimate(1),2)];
+R2 = num2str(mdl.Rsquared.Ordinary,2);
+
+figure(5),clf
+h = plot(mdl,'marker','.');
+delete(h([3 4]))    % Delete confidence bounds on plot
+legend('Data',[eqn,newline,'R^2 = ',R2])
+xlabel('Gull Met Station (Cleaned)')
+ylabel('NOAA - Atlantic City')
+title('Daily Mean Wind Speed (m/s)')
+daspect([1 1 1])
+
+%====Save the cleaned data=================================================
+cd('G:\My Drive\Postdoc\Work\SMIIL\physical-data\wind-speed')
+save('windSpeed.mat','metDat_cleaned','metDat_orig','noaaDat')
